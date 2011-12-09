f1fb21b21412531a462323f6e2b9f557975d4a38
Matt Petty <matt@kizmeta.com>
2011-12-09 15:12:11 -0600

    Inital styling

--- js.js
+++ js.js
@@ -1,1 +1,14 @@
-alert("Loaded Lodestone Mod");
+  WebFontConfig = {
+    google: { families: [ 'Electrolize::latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })();
+
+
